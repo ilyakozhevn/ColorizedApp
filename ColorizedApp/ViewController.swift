20709e77073ad37b6566811904715420cd508755
ilyakozhevn <kozhevilya@gmail.com>
2022-06-25 08:09:12 +0300

    First working prototype

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    MARK: initialisation of variables
+    @IBOutlet var colorScreenView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorScreenView.layer.cornerRadius = 15
+        colorScreenAdjustment()
+        }
+    
+    @IBAction func redSliderChange() {
+        textChange(of: redValueLabel, to: redSlider.value)
+        colorScreenAdjustment()
+    }
+    
+    @IBAction func greenSliderChange() {
+        textChange(of: greenValueLabel, to: greenSlider.value)
+        colorScreenAdjustment()
+    }
+    @IBAction func blueSliderChange() {
+        textChange(of: blueValueLabel, to: blueSlider.value)
+        colorScreenAdjustment()
+    }
+    
+//   MARK: private functions
+    private func getCGFloat(from label: UILabel) -> CGFloat {
+        CGFloat(Float(label.text ?? "0") ?? 0)
     }
 
+    private func colorScreenAdjustment() {
 
+        colorScreenView.backgroundColor = UIColor(
+            red: getCGFloat(from: redValueLabel),
+            green: getCGFloat(from: greenValueLabel),
+            blue: getCGFloat(from: blueValueLabel),
+            alpha: CGFloat(1)
+        )
+    }
+    
+    private func textChange(of label: UILabel, to value: Float) {
+        label.text = String( (value * 100).rounded() / 100)
+    }
 }
 
