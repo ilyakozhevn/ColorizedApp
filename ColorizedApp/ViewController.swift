8a8f425de0eda8890b826fa937d4363d3bc6914b
ilyakozhevn <kozhevilya@gmail.com>
2022-06-25 15:31:26 +0300

    Initial Colors are sent to sliders

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -26,8 +26,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         colorScreenView.layer.cornerRadius = 15
-        colorScreenAdjustment()
-        }
+        getInitialColor()
+    }
     
 //    MARK: sliders change functions
     
@@ -47,6 +47,42 @@ class ViewController: UIViewController {
     
 //   MARK: private functions
     
+    private func getInitialColor() {
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        colorScreenView.backgroundColor!.getRed(
+            &red,
+            green: &green,
+            blue: &blue,
+            alpha: &alpha
+        )
+        
+        initialSlidersPosition(
+            red: Float(red),
+            green: Float(green),
+            blue: Float(blue)
+        )
+
+    }
+    
+    private func initialSlidersPosition(red: Float, green: Float, blue: Float) {
+        redSlider.value = red
+        greenSlider.value = green
+        blueSlider.value = blue
+        
+        textChange(of: redValueLabel, to: red)
+        textChange(of: greenValueLabel, to: green)
+        textChange(of: blueValueLabel, to: blue)
+    }
+    
+    private func textChange(of label: UILabel, to value: Float) {
+        label.text = String(format: "%.2f", value)
+    }
+    
     private func colorScreenAdjustment() {
 
         colorScreenView.backgroundColor = UIColor(
@@ -55,10 +91,6 @@ class ViewController: UIViewController {
             blue: CGFloat(blueSlider.value),
             alpha: CGFloat(1)
         )
-    }
-    
-    private func textChange(of label: UILabel, to value: Float) {
-        label.text = String(format: "%.2f", value)
     }
 }
 
